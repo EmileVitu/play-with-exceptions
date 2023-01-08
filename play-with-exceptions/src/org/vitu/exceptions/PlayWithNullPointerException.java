ddb57e8ed5e915277d8ef49fa9b8bc8d9798b596
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-01-08 16:39:28 +0100

    End of the work on NPE and Exceptions

--- play-with-exceptions/src/org/vitu/exceptions/PlayWithNullPointerException.java
+++ play-with-exceptions/src/org/vitu/exceptions/PlayWithNullPointerException.java
@@ -8,7 +8,11 @@ public class PlayWithNullPointerException {
 		
 		Commune commune = new Commune();
 		
-		int length = commune.getMaire().getAdresse().getRue().length();
+		int length = commune.getMaire().getAdresse().getRue().length(); 
+		// --> New feature of Java 14 : NPE are more detailled for debug
+		// Exception in thread "main" java.lang.NullPointerException: 
+		// Cannot invoke "org.vitu.exceptions.model.Maire.getAdresse()" because the return value of "org.vitu.exceptions.model.Commune.getMaire()" is null
+		// at org.vitu.exceptions.PlayWithNullPointerException.main(PlayWithNullPointerException.java:11)
 		System.out.println("Length = " + length);
 
 	}
